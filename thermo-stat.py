683a96c652255e72a55bb6df25ee9d662f1efe59
Luke Forehand <luke.forehand@networkedinsights.com>
2015-04-26 14:37:50 -0500

    parameterized location, further specified values

--- thermo-stat.py
+++ thermo-stat.py
@@ -7,6 +7,7 @@ import urllib2
 graphite_host = 'graphite.colo.networkedinsights.com'
 graphite_port = 2013
 json_source = 'https://www.lacrossealerts.com/v1/observations/9509?format=json&from=-1days'
+device='luke.porch'
 auth_cookie = sys.argv[1]
 
 opener = urllib2.build_opener()
@@ -14,14 +15,11 @@ opener.addheaders.append(('Cookie', auth_cookie))
 data = json.load(opener.open(json_source))
 sock = socket.socket()
 sock.connect((graphite_host, graphite_port))
+message = ""
 for item in data.get('response').get('obs'):
-  message = 'luke.porch.probe ' + `item.get('values').get('temp')` + ' ' + `item.get('timeStamp')` + '\n'
-  print message
-  sock.sendall(message)
-  message = 'luke.porch.ambient ' + `item.get('values').get('temp2')` + ' ' + `item.get('timeStamp')` + '\n'
-  print message
-  sock.sendall(message)
-  message = 'luke.porch.humidity ' + `item.get('values').get('rh')` + ' ' + `item.get('timeStamp')` + '\n'
-  print message
-  sock.sendall(message)
+  message = message + device + '.probe.temp ' + `item.get('values').get('temp')` + ' ' + `item.get('timeStamp')` + '\n'
+  message = message + device + '.ambient.temp ' + `item.get('values').get('temp2')` + ' ' + `item.get('timeStamp')` + '\n'
+  message = message + device + '.humidity.pct ' + `item.get('values').get('rh')` + ' ' + `item.get('timeStamp')` + '\n'
+print message
+sock.sendall(message)
 sock.close()
